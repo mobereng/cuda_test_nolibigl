adba4fd595a9be4a965d3cae448c48e5d53c87eb
Moisés Berenguer <ext_mobereng@mercadolibre.com.ve>
2021-02-15 22:48:02 -0400

    Minimal version

--- compile.ps1
+++ compile.ps1
@@ -1,4 +1,4 @@
 New-Item -ItemType Directory -Force -Path build/
 Remove-Item build/* -Recurse -Confirm:$false -exclude .gitkeep
-cmake -S . -B build/ -DCMAKE_GENERATOR_PLATFORM=x64
+cmake -S . -B build/ -DCMAKE_GENERATOR_PLATFORM=x64 -DEigen3_DIR="../eigen/share/eigen3/cmake/"
 MSBuild.exe build/CudaTestNoLibIgl.sln -property:Configuration=Release -m
